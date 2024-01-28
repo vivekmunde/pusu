52941449ca970b593ac8be5dcd90cccd8f9ad9d3
Vivek <vivekmunde@users.noreply.github.com>
2024-01-28 14:53:13 +0530

    Redefined types

--- src/index.ts
+++ src/index.ts
@@ -1,51 +1,28 @@
 /**
+ * A subscriber function to be called each time the data is published.
+ *
+ * @param {T} data - The data sent by the publisher.
+ */
+export type TSubscriber<T> = (data: T) => void;
+
+/**
  * Publication object.
+ *
+ * @property {string} name - Name of the publication.
+ * @property {TSubscriber[]} subscribers - Collection of subscriber functions currently subscribed to the publication.
  */
 export type TPublication<T> = {
-  /* Name of the publication. */
   name?: string;
-
-  /* Collection of subscriber functions currently subscribed to the publication. */
-  subscribers: ((data: T) => void)[];
+  subscribers: TSubscriber<T>[];
 };
 
 /**
  * Creates a new publication.
  *
  * @param {string} name - Name of the publication. Helpful in debugging and logging.
- * @returns {TPublication<T>} Publication object containing the pub-sub functions.
+ * @returns {TPublication} Publication.
  */
-export type TCreatePublication = <T>(
-  /* Name of the publication. */
-  name?: string
-) => TPublication<T>;
-
-export type TPublish = <T>(
-  /** Publication object. */
-  publication: TPublication<T>,
-
-  /** Data to be published and to be sent to all subscribers. */
-  data: T
-) => void;
-
-/** Subscribes to the publication. */
-export type TSubscribe = <T>(
-  /** Publication object. */
-  publication: TPublication<T>,
-
-  /** A subscriber function to be called each time the data is published. */
-  subscriber: (
-    /** The data sent by the publisher. */
-    data: T
-  ) => void
-) => () => void;
-
-/**
- * Creates a new publication.
- *
- * @param {string} name - Name of the publication. Helpful in debugging and logging.
- */
-export const createPublication: TCreatePublication = (name) => {
+export const createPublication = <T>(name?: string): TPublication<T> => {
   return {
     name,
     subscribers: [],
@@ -58,18 +35,24 @@ export const createPublication: TCreatePublication = (name) => {
  * @param {TPublication} publication - Publication object.
  * @param {T} data - Data to publish and to be sent to all subscribers.
  */
-export const publish: TPublish = (publication, data) => {
+export const publish = <T>(publication: TPublication<T>, data: T): void => {
   for (const subscriber of publication.subscribers) {
     subscriber(data);
   }
 };
 
-/** Subscribes to the publication.
+/**
+ * Subscribes to the publication.
  *
  * @param {TPublication} publication - Publication object.
- * @param {function} subscriber - A subscriber function to be called each time the data is published.
+ * @param {TSubscriber} subscriber - A subscriber function to be called each time the data is published.
+ *
+ * @returns {function} Ubsubscribes the subscriber from the publication.
  */
-export const subscribe: TSubscribe = (publication, subscriber) => {
+export const subscribe = <T>(
+  publication: TPublication<T>,
+  subscriber: TSubscriber<T>
+): (() => void) => {
   const { subscribers } = publication;
 
   subscribers.push(subscriber);
