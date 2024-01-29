18c13babe9f5c9602accb523356124de43088e76
Vivek <vivekmunde@users.noreply.github.com>
2024-01-29 12:38:08 +0530

    Implement logging

--- src/index.ts
+++ src/index.ts
@@ -6,27 +6,79 @@
 export type TSubscriber<T> = (data: T) => void;
 
 /**
+ * Configuration options to be provided to the publication being created.
+ */
+export type TCreateConfiguration = {
+  /** Used in logging. Default: "Unknown". */
+  name?: string;
+
+  /** Enable/disable console logging. Useful in development and/or test environments. If enabled then each action "create" | "publish" | "subscribe" | "unsubscribe" | "notify" gets logged on console with relevent data. */
+  enableLogging?: boolean;
+};
+
+/**
  * Publication object.
  *
- * @property {string} name - Name of the publication.
+ * @property {TCreateConfiguration} config - Configuration options provided at the time of creating the publication.
  * @property {TSubscriber[]} subscribers - Collection of subscriber functions currently subscribed to the publication.
  */
 export type TPublication<T> = {
-  name?: string;
+  config?: TCreateConfiguration;
   subscribers: TSubscriber<T>[];
+};
+
+type TLogAction = "create" | "publish" | "subscribe" | "unsubscribe" | "notify";
+
+/**
+ * Logs the action and its relative information.
+ *
+ * @param {TCreateConfiguration} config - Configuration options provided at the time of creating the publication.
+ * @param {TLogAction} action - Action: "create" | "publish" | "subscribe" | "unsubscribe" | "notify"
+ * @param {TData} data - Data sent to subscribers.
+ * @param {TMetaData} meta - Can be any metadata. E.g. when a subscriber is notified then the sibscriber function's name is added in the metadata. */
+const log = <TData, TMetaData>(
+  config: TCreateConfiguration | undefined,
+  action: TLogAction,
+  data?: TData,
+  meta?: TMetaData
+) => {
+  if (config?.enableLogging) {
+    const info: {
+      publication: string;
+      action: TLogAction;
+      data?: TData;
+      meta?: TMetaData;
+    } = { publication: config.name ?? "Unknown", action };
+
+    if (data) {
+      info.data = data;
+    }
+
+    if (meta) {
+      info.meta = meta;
+    }
+
+    console.log("pusu", info);
+  }
 };
 
 /**
  * Creates a new publication.
  *
- * @param {string} name - Name of the publication. Helpful in debugging and logging.
+ * @param {TCreateConfiguration} config - Configuration options to be provided to the publication being created.
  * @returns {TPublication} Publication.
  */
-export const createPublication = <T>(name?: string): TPublication<T> => {
-  return {
-    name,
+export const createPublication = <T>(
+  config?: TCreateConfiguration
+): TPublication<T> => {
+  const publication: TPublication<T> = {
+    config,
     subscribers: [],
   };
+
+  log(config, "create", undefined);
+
+  return publication;
 };
 
 /**
@@ -36,7 +88,13 @@ export const createPublication = <T>(name?: string): TPublication<T> => {
  * @param {T} data - Data to publish and to be sent to all subscribers.
  */
 export const publish = <T>(publication: TPublication<T>, data: T): void => {
+  log<T, undefined>(publication.config, "publish", data, undefined);
+
   for (const subscriber of publication.subscribers) {
+    log<T, { subscriber: string }>(publication.config, "notify", data, {
+      subscriber: subscriber.name,
+    });
+
     subscriber(data);
   }
 };
@@ -47,17 +105,35 @@ export const publish = <T>(publication: TPublication<T>, data: T): void => {
  * @param {TPublication} publication - Publication object.
  * @param {TSubscriber} subscriber - A subscriber function to be called each time the data is published.
  *
- * @returns {function} Ubsubscribes the subscriber from the publication.
+ * @returns {function} A function to ubsubscribe the subscriber from the publication.
  */
 export const subscribe = <T>(
   publication: TPublication<T>,
   subscriber: TSubscriber<T>
 ): (() => void) => {
+  log<undefined, { subscriber: string }>(
+    publication.config,
+    "subscribe",
+    undefined,
+    {
+      subscriber: subscriber.name,
+    }
+  );
+
   const { subscribers } = publication;
 
   subscribers.push(subscriber);
 
   return function unsubscribe() {
+    log<undefined, { subscriber: string }>(
+      publication.config,
+      "unsubscribe",
+      undefined,
+      {
+        subscriber: subscriber.name,
+      }
+    );
+
     const index = subscribers.indexOf(subscriber);
 
     if (index > -1) {
--- test/pusu.test.js
+++ test/pusu.test.js
@@ -1,27 +1,22 @@
 import { createPublication, publish, subscribe } from "../lib/es";
 
 describe("pub-sub", () => {
-  test(`Should return new publication object without name`, () => {
-    expect.hasAssertions();
-    expect(createPublication()).toEqual({ name: undefined, subscribers: [] });
-  });
-
   test("Should return a unique new publication object every time", () => {
     expect.hasAssertions();
 
-    const publication1 = createPublication("publication-name");
-    const publication2 = createPublication("publication-name");
-    const publication3 = createPublication("publication-name");
+    const publication1 = createPublication({ name: "publication" });
+    const publication2 = createPublication({ name: "publication" });
+    const publication3 = createPublication({ name: "publication" });
 
     expect(publication1 === publication2).toBe(false);
     expect(publication1 === publication3).toBe(false);
     expect(publication2 === publication3).toBe(false);
   });
 
-  test("Should not error when publishing a publication without any subscribers", () => {
+  test("Should not error when publishing without any subscribers", () => {
     expect.hasAssertions();
 
-    const publication = createPublication("test");
+    const publication = createPublication({ name: "test" });
 
     publish(publication, undefined);
 
@@ -31,7 +26,7 @@ describe("pub-sub", () => {
   test("Should publish & call the subscribers without data", () => {
     expect.hasAssertions();
 
-    const publication = createPublication("test");
+    const publication = createPublication({ name: "test" });
 
     const subscriber1 = jest.fn((val) => val);
     const subscriber2 = jest.fn((val) => val);
@@ -55,7 +50,7 @@ describe("pub-sub", () => {
     const testData2 = { data: "test-data2" };
     const testData3 = ["test-data3"];
 
-    const publication = createPublication("test");
+    const publication = createPublication({ name: "test" });
 
     const subscriber1 = jest.fn((val) => val);
     const subscriber2 = jest.fn((val) => val);
@@ -83,7 +78,7 @@ describe("pub-sub", () => {
   test("Should not call the subscriber after unsubscribing", () => {
     expect.hasAssertions();
 
-    const publication = createPublication("test");
+    const publication = createPublication({ name: "test" });
 
     const subscriber = jest.fn((val) => val);
 
@@ -103,7 +98,7 @@ describe("pub-sub", () => {
   test("Should not error on multiple calls to unsubscribe", () => {
     expect.hasAssertions();
 
-    const publication = createPublication("test");
+    const publication = createPublication({ name: "test" });
 
     const subscriber = jest.fn((val) => val);
 
@@ -125,7 +120,7 @@ describe("pub-sub", () => {
   test("Should remove subscribers on unsubscribe", () => {
     expect.hasAssertions();
 
-    const publication = createPublication("test");
+    const publication = createPublication({ name: "test" });
 
     const subscriber1 = jest.fn((val) => val);
     const subscriber2 = jest.fn((val) => val);
@@ -150,5 +145,163 @@ describe("pub-sub", () => {
 
     expect(subscriber1).toHaveBeenCalledTimes(1);
     expect(subscriber2).toHaveBeenCalledTimes(2);
+  });
+
+  test("Should not log when flag is not passed", () => {
+    expect.hasAssertions();
+
+    const logSpy = jest.spyOn(console, "log");
+
+    const testData = "test-data";
+
+    const publication = createPublication();
+
+    expect(logSpy).not.toHaveBeenCalled();
+
+    const subscriber = jest.fn((val) => val);
+
+    const unsubscribe = subscribe(publication, subscriber);
+
+    expect(logSpy).not.toHaveBeenCalled();
+
+    publish(publication, testData);
+
+    expect(logSpy).not.toHaveBeenCalled();
+
+    expect(subscriber).toHaveBeenCalledWith(testData);
+
+    unsubscribe();
+  });
+
+  test("Should not log when flag is passed", () => {
+    expect.hasAssertions();
+
+    const logSpy = jest.spyOn(console, "log");
+
+    const testData = "test-data";
+
+    const publication = createPublication({
+      name: "test",
+      enableLogging: false,
+    });
+
+    expect(logSpy).not.toHaveBeenCalled();
+
+    const subscriber = jest.fn((val) => val);
+
+    const unsubscribe = subscribe(publication, subscriber);
+
+    expect(logSpy).not.toHaveBeenCalled();
+
+    publish(publication, testData);
+
+    expect(logSpy).not.toHaveBeenCalled();
+
+    expect(subscriber).toHaveBeenCalledWith(testData);
+
+    unsubscribe();
+  });
+
+  test("Should log", () => {
+    expect.hasAssertions();
+
+    const logSpy = jest.spyOn(console, "log");
+
+    const testData1 = "test-data1";
+    const testData2 = { data: "test-data2" };
+    const testData3 = ["test-data3"];
+
+    const publication = createPublication({
+      name: "test",
+      enableLogging: true,
+    });
+
+    expect(logSpy).toHaveBeenCalledWith("pusu", {
+      publication: "test",
+      action: "create",
+    });
+
+    const subscriber1 = jest.fn((val) => val);
+    const subscriber2 = jest.fn((val) => val);
+
+    const unsubscribe1 = subscribe(publication, subscriber1);
+
+    expect(logSpy).toHaveBeenCalledWith("pusu", {
+      publication: "test",
+      action: "subscribe",
+      meta: { subscriber: "mockConstructor" },
+    });
+
+    const unsubscribe2 = subscribe(publication, subscriber2);
+
+    expect(logSpy).toHaveBeenCalledWith("pusu", {
+      publication: "test",
+      action: "subscribe",
+      meta: { subscriber: "mockConstructor" },
+    });
+
+    publish(publication, { testData1, testData2, testData3 });
+
+    expect(logSpy).toHaveBeenCalledWith("pusu", {
+      publication: "test",
+      action: "publish",
+      data: {
+        testData1: "test-data1",
+        testData2: { data: "test-data2" },
+        testData3: ["test-data3"],
+      },
+    });
+
+    expect(subscriber1).toHaveBeenCalledWith({
+      testData1,
+      testData2,
+      testData3,
+    });
+
+    expect(subscriber2).toHaveBeenCalledWith({
+      testData1,
+      testData2,
+      testData3,
+    });
+
+    unsubscribe1();
+    unsubscribe2();
+  });
+
+  test("Should log default name", () => {
+    expect.hasAssertions();
+
+    const logSpy = jest.spyOn(console, "log");
+
+    const testData = "test-data";
+
+    const publication = createPublication({ enableLogging: true });
+
+    expect(logSpy).toHaveBeenCalledWith("pusu", {
+      publication: "Unknown",
+      action: "create",
+    });
+
+    const subscriber = jest.fn((val) => val);
+
+    const unsubscribe = subscribe(publication, subscriber);
+
+    expect(logSpy).toHaveBeenCalledWith("pusu", {
+      publication: "Unknown",
+      action: "subscribe",
+      meta: { subscriber: "mockConstructor" },
+    });
+
+    publish(publication, testData);
+
+    expect(logSpy).toHaveBeenCalledWith("pusu", {
+      publication: "Unknown",
+      action: "publish",
+      data: "test-data",
+    });
+
+    expect(subscriber).toHaveBeenCalledWith(testData);
+
+    unsubscribe();
   });
 });
